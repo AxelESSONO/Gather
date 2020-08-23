9f8f288b169a35349c20a71dcc82f3da3274e68d
Axel ESSONO <naxelcdric@gmail>
2020-08-23 18:53:25 +0200

    Updated FilterSource

--- app/src/main/java/com/obiangetfils/gather/controllers/MainActivity.java
+++ app/src/main/java/com/obiangetfils/gather/controllers/MainActivity.java
@@ -3,10 +3,16 @@ package com.obiangetfils.gather.controllers;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +24,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.obiangetfils.gather.R;
 import com.obiangetfils.gather.adapters.AdapterSourcelist;
 import com.obiangetfils.gather.models.Constants;
@@ -38,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<ModelSourceList> sourceLists;
     private AdapterSourcelist adapterSourcelist;
 
+    private String selectedCountry = "All", selectedCategories = "All", selectedLanguage = "All";
+    private int selectedCountryPosition = 0, selectedCategoryPosition = 0, selectedLanguagePosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,16 +81,125 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterBottomSheet();
+            }
+        });
+
+    }
+
+    private void filterBottomSheet() {
+        View view = LayoutInflater.from(this).inflate(R.layout.filter_layout, null);
+        Spinner countrySpinner, categorySpinner, languageSpinner;
+        TextView countryLabelTv, categoryLabelTv, languageLabelTv;
+        Button applyBtn;
+
+        countrySpinner = (Spinner) view.findViewById(R.id.countrySpinner);
+        categorySpinner = (Spinner) view.findViewById(R.id.categorySpinner);
+        languageSpinner = (Spinner) view.findViewById(R.id.languageSpinner);
+
+        countryLabelTv = (TextView) view.findViewById(R.id.countryLabelTv);
+        categoryLabelTv = (TextView) view.findViewById(R.id.categoryLabelTv);
+        languageLabelTv = (TextView) view.findViewById(R.id.languageLabelTv);
+
+        applyBtn = (Button) view.findViewById(R.id.applyBtn);
+
+        ArrayAdapter<String> adapterCountries = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Constants.COUNTRIES);
+        ArrayAdapter<String> adapterCategories = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Constants.CATEGORIES);
+        ArrayAdapter<String> adapterLanguages = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Constants.LANGUAGES);
+
+        adapterCountries.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        adapterCategories.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        adapterLanguages.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        countrySpinner.setAdapter(adapterCountries);
+        categorySpinner.setAdapter(adapterCategories);
+        languageSpinner.setAdapter(adapterLanguages);
+
+        countrySpinner.setSelection(selectedCountryPosition);
+        categorySpinner.setSelection(selectedCategoryPosition);
+        languageSpinner.setSelection(selectedLanguagePosition);
+
+        //spinner item selected listener
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = Constants.COUNTRIES[position];
+                selectedCountryPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCategories = Constants.CATEGORIES[position];
+                selectedCategoryPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedLanguage = Constants.LANGUAGES[position];
+                selectedLanguagePosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.show();
+
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+                loadSoures();
+            }
+        });
 
     }
 
     private void loadSoures() {
+
+        //show selected filter option in actionBar
+        getSupportActionBar().setSubtitle("Country: "+selectedCountry+ " Category: "+ selectedCategories+ " Language: "+selectedLanguage);
+
+        if (selectedCountry.equals("All")){
+            selectedCountry = "";
+        }
+
+        if (selectedCategories.equals("All")){
+            selectedCategories = "";
+        }
+
+        if (selectedLanguage.equals("All")){
+            selectedLanguage = "";
+        }
+
+
         sourceLists = new ArrayList<>();
         sourceLists.clear();
 
         progressBar.setVisibility(View.VISIBLE);
         //Request Data
-        String url = "https://newsapi.org/v2/sources?apiKey=" + Constants.API_KEY;
+        String url = "https://newsapi.org/v2/sources?apiKey=" + Constants.API_KEY+"&country=" +selectedCountry + "&category"+selectedCategories+ "&language"+selectedLanguage;
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -94,8 +212,7 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray jsonArray = jsonObject.getJSONArray("sources");
 
                     //Get All data from that Array using loop
-                    for (int i = 0; i < jsonArray.length(); i++)
-                    {
+                    for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject1 = jsonArray.getJSONObject(i);
 
                         String id = jsonObject1.getString("id");
--- app/src/main/java/com/obiangetfils/gather/models/Constants.java
+++ app/src/main/java/com/obiangetfils/gather/models/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
 
     public static final String API_KEY = "f292511d18194c7595b9aadd7f7f676e";
 
+    public static final String[] COUNTRIES = {"All", "ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"};
+    public static final String[] LANGUAGES = {"All", "ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "se", "ud", "zh"};
+    public static final String[] CATEGORIES = {"All", "business", "entertainment", "general", "health", "science", "sports", "technology"};
+
 }
